f871bd1baef747e1969196bf88f24a8f2fa59970
hudeintres <nodadfat@gmail.com>
2024-07-09 02:32:04 +0000

    find root dir dynamically

--- tests/utils/general.ts
+++ tests/utils/general.ts
@@ -9,8 +9,22 @@ export const EMPTY_WITNESS = [];
 export const TEST_BTC_ADDRESS1 = "16aE44Au1UQ5XqKMUhCMXTX7ZxbmAcQNA1";
 export const TEST_BTC_ADDRESS2 = "1AdAhGdUgGF6ip7bBcVvuWYuuCxAeonNaK";
 
+function findRootDir(currentDir: string): string {
+  const parentDir = path.resolve(currentDir, '..');
+
+  if (fs.existsSync(path.join(currentDir, 'package.json'))) {
+    return currentDir;
+  } else if (currentDir === parentDir) {
+    // If we reach the root directory of the file system, throw an error.
+    throw new Error('Root directory not found');
+  } else {
+    // Recur with the parent directory.
+    return findRootDir(parentDir);
+  }
+}
+
 export const DEBUG_WASM = fs.readFileSync(
-  path.join(__dirname, "..", "..", "build", "debug.wasm"),
+  path.join(findRootDir(__dirname), "build", "debug.wasm"),
 );
 
 export const log = (obj: any) => {
