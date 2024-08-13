91d6396aa96756c6ac5b8514309aecfbc6f760cb
v16s <16210686+v16s@users.noreply.github.com>
2024-08-14 00:36:58 +0530

    add block skip functionality

--- lib/tests/utils/rune-helpers.d.ts
+++ lib/tests/utils/rune-helpers.d.ts
@@ -1,7 +1,7 @@
 import bitcoinjs = require("bitcoinjs-lib");
 import { IndexerProgram } from "metashrew-test";
 export declare const runesbyaddress: (program: IndexerProgram, address: string) => Promise<any>;
-export declare const initCompleteBlockWithRuneEtching: (outputs: any, pointer: number, divisibility?: number, premineAmount?: bigint, runeName?: string, symbol?: string, block?: bitcoinjs.Block) => bitcoinjs.Block;
+export declare const initCompleteBlockWithRuneEtching: (outputs: any, pointer: number, divisibility?: number, premineAmount?: bigint, runeName?: string, symbol?: string, block?: bitcoinjs.Block, skipTx?: number) => bitcoinjs.Block;
 export declare const transferRune: (inputs: {
     inputTxHash: Buffer | undefined;
     inputTxOutputIndex: number;
