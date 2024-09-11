0531d5f6f5249e689b21b2c9518f00b88fd81c79
phuncti0n <zmill777@proton.me>
2024-09-11 11:48:17 -0400

    add buffer

--- src.ts/wallet.ts
+++ src.ts/wallet.ts
@@ -2,7 +2,7 @@ import { WalletResponse, WalletRequest } from "./proto/metashrew-runes";
 import { decodeOutpointViewBase } from "./outpoint";
 import { decodeRunes, OutPoint, RuneOutput } from "./outpoint";
 import { stripHexPrefix } from "./utils";
-
+import { Buffer } from "buffer";
 
 export function decodeWalletOutput(hex: string): {
   outpoints: OutPoint[];
