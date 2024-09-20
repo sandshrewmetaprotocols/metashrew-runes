19c97dcd7f8c82425d061437a34eeed33f83e2c9
sprimage <timilehin1999@gmail.com>
2024-09-20 12:58:10 -0600

    runesbyblock rpc

--- lib/src.ts/rpc.d.ts
+++ lib/src.ts/rpc.d.ts
@@ -11,4 +11,13 @@ export declare class MetashrewRunes {
         outpoints: OutPoint[];
         balanceSheet: RuneOutput[];
     }>;
+    runesByBlock(height: number): Promise<{
+        runes: Array<{
+            runeId: string;
+            name: string;
+            divisibility: number;
+            spacers: number;
+            symbol: string;
+        }>;
+    }>;
 }
